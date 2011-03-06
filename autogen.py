807fae3a082fbc7cb89959bddd2e216954dae4b8
C Anthony Risinger <anthony@extof.me>
2011-03-06 02:43:34 -0600

    WIP:checkpoint:cleaning lib dir, removing compat15

--- autogen.py
+++ autogen.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python2
 
-
+import sys
+import os
 import re
 import urllib
 import tarfile
 import StringIO
 from hashlib import sha1
+from optparse import OptionParser
 
 
 IDENT = 'waf-1.6.3'
 SHA1 = '86000f9349009340ea4124adf4ac1d167c6e012c'
-LOCATION = 'http://waf.googlecode.com/files/{ident}.tar.bz2'.format(ident=IDENT)
+REMOTE = 'http://waf.googlecode.com/files/{ident}.tar.bz2'.format(ident=IDENT)
 
 INCLUDE = {
-    '/':                r'waf-light',
-    '/waflib/':         r'[^/]*\.py',
-    '/waflib/Tools/':   r'(__init__|python|errcheck|gnu_dirs)\.py',
-    '/waflib/extras/':  r'(__init__|compat15|parallel_debug|why)\.py',
+    '/': r'(waf-light|wscript)',
+    '/waflib/': r'[^/]*\.py',
+    '/waflib/Tools/': r'(__init__|python|errcheck|gnu_dirs)\.py',
+    '/waflib/extras/': r'(__init__|compat15|parallel_debug|why)\.py',
 }
 
 RENAME = {
-    '^waf-light$':      r'pj-light',
-    '^waflib':          r'pjlib',
-    'wscript$':         r'pjscript',
+    '^waf-light$': r'pjswaf-light',
+    '^waflib': r'pjswaflib',
+    'wscript$': r'pjscript',
 }
 
 TRANSFORM = {
-    'waf':              r'pj',
-    'Waf':              r'Pj',
-    'WAF':              r'PJ',
-    'wscript':          r'pjscript',
+    'waf': r'pjswaf',
+    'Waf': r'Pjswaf',
+    'WAF': r'PJSWAF',
+    'wscript': r'pjscript',
+    '^.*compat15#PRELUDE.*$': r'',
 }
 
 
@@ -79,11 +82,25 @@ def transform(m, fd):
 
 
 if __name__ == '__main__':
-    waf = urllib.urlopen(LOCATION).read()
-    if sha1(waf).hexdigest() != SHA1:
-        raise ValueError('Waf archive is corrupt.')
+    parser = OptionParser()
+    parser.add_option('-w', '--waf', metavar='URI', default=REMOTE,
+        help="retrieve {ident} from URI [%default]".format(ident=IDENT))
+    parser.add_option('-s', '--sha1', default=SHA1,
+        help="expected SHA1 of URI [%default]".format(ident=IDENT))
+    opts, args = parser.parse_args()
+    base = os.path.dirname(os.path.abspath(sys.argv[0] or '.'))
+    build = os.path.join(base, 'build')
+    buildlib = os.path.join(build, 'lib')
+    if not os.access(build, os.R_OK|os.W_OK):
+        os.mkdir(build)
+    os.chdir(build)
+    wafz = urllib.urlretrieve(opts.waf, IDENT+'.tar.bz2')[0]
     pj = StringIO.StringIO()
-    waf = StringIO.StringIO(waf)
+    with open(wafz, 'rb') as w:
+        if sha1(w.read()).hexdigest() != opts.sha1:
+            raise ValueError('Waf archive is corrupted.')
+        w.seek(0)
+        waf = StringIO.StringIO(w.read())
     pjball = tarfile.open(mode='w:gz', fileobj=pj)
     wafball = tarfile.open(fileobj=waf)
     for m in wafball.getmembers():
@@ -94,6 +111,6 @@ if __name__ == '__main__':
     pjball.close()
     pj.seek(0)
     pjball = tarfile.open(fileobj=pj)
-    pjball.extractall()
+    pjball.extractall(buildlib)
     for fd in pjball, wafball, pj, waf:
         fd.close()
