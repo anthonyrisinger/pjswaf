1d0adfdbbbce3d64c632f2c9f9597a30f081ab11
C Anthony Risinger <anthony@extof.me>
2011-03-08 00:02:40 -0600

    cache waf archive; prefer local version

--- autogen.py
+++ autogen.py
@@ -12,13 +12,15 @@ from optparse import OptionParser
 
 IDENT = 'waf-1.6.3'
 SHA1 = '86000f9349009340ea4124adf4ac1d167c6e012c'
-REMOTE = 'http://waf.googlecode.com/files/{ident}.tar.bz2'.format(ident=IDENT)
+REMOTE = 'http://waf.googlecode.com/files'
+
+ARCHIVE = '{ident}.tar.bz2'.format(ident=IDENT)
 
 INCLUDE = {
     '/': r'(waf-light|wscript)',
     '/waflib/': r'[^/]*\.py',
     '/waflib/Tools/': r'(__init__|python|errcheck|gnu_dirs)\.py',
-    '/waflib/extras/': r'(__init__|compat15|parallel_debug|why)\.py',
+    '/waflib/extras/': r'(__init__|parallel_debug|why)\.py',
 }
 
 RENAME = {
@@ -26,7 +28,7 @@ RENAME = {
     '^waflib': r'pjswaflib',
     'wscript$': r'pjscript',
 }
-
+#FIXME PRELUDE must exist
 TRANSFORM = {
     'waf': r'pjswaf',
     'Waf': r'Pjswaf',
@@ -82,25 +84,29 @@ def transform(m, fd):
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-w', '--waf', metavar='URI', default=REMOTE,
-        help="retrieve {ident} from URI [%default]".format(ident=IDENT))
-    parser.add_option('-s', '--sha1', default=SHA1,
-        help="expected SHA1 of URI [%default]".format(ident=IDENT))
-    opts, args = parser.parse_args()
     base = os.path.dirname(os.path.abspath(sys.argv[0] or '.'))
     build = os.path.join(base, 'build')
-    buildlib = os.path.join(build, 'lib')
+    buildlib = os.path.join(build, 'pjswaf')
+    parser = OptionParser()
+    parser.add_option('-w', '--waf', metavar='URI', default=os.path.join(build, ARCHIVE),
+        help="retrieve {ident} from URI [{build}, {remote}]".format(ident=IDENT, build=build, remote=REMOTE))
+    parser.add_option('-s', '--sha1', default=SHA1,
+        help="expected SHA1 of URI [%default]")
+    opts, args = parser.parse_args()
     if not os.access(build, os.R_OK|os.W_OK):
         os.mkdir(build)
     os.chdir(build)
-    wafz = urllib.urlretrieve(opts.waf, IDENT+'.tar.bz2')[0]
+    try:
+        with open(opts.waf, 'rb'):
+            wafz = opts.waf
+    except IOError:
+        wafz = urllib.urlretrieve(REMOTE + '/' + ARCHIVE, ARCHIVE)[0]
     pj = StringIO.StringIO()
     with open(wafz, 'rb') as w:
-        if sha1(w.read()).hexdigest() != opts.sha1:
-            raise ValueError('Waf archive is corrupted.')
-        w.seek(0)
         waf = StringIO.StringIO(w.read())
+        if sha1(waf.read()).hexdigest() != opts.sha1:
+            raise ValueError('Waf archive is corrupted.')
+        waf.seek(0)
     pjball = tarfile.open(mode='w:gz', fileobj=pj)
     wafball = tarfile.open(fileobj=waf)
     for m in wafball.getmembers():
